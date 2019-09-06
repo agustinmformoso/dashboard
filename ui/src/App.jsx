ae845ad12f8e21768f4adc6a1243fe02d285865b
Agustin Formoso <agustinf@lagash.com>
2019-09-06 15:52:08 -0300

    Estructura basica App

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import "./styles.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row>
+                <Col md={9} className="header" >
+                    
+                </Col>
+                
+                <Col md={3} className="buttons" >
+                    
+                </Col>
+            </Row>
+
+            <Row>
+                <Col md={3} className="panel" >
+                    
+                </Col>
+
+                <Col md={9}>
+                    <Row>
+                        <Col md={3} className="chart-1" >
+                        
+                        </Col>
+
+                        <Col md={2} className="chart-2" >
+                        
+                        </Col>
+
+                        <Col md={6} className="chart-3" >
+                        
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col md={12} className="grid" >
+
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
