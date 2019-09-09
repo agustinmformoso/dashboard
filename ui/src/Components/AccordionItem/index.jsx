e7a968e81899a260a2e64dd75684950fc3dad2a4
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-09 16:08:04 -0300

    accordion style

--- ui/src/Components/AccordionItem/index.jsx
+++ ui/src/Components/AccordionItem/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./styles.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const AccordionItem = ({ t }) => {
     return (
-        <div>
-            <p>{t.name} {t.last_name}</p>
-            <p>{t.rol}</p>
+        <div className="itemContainer p-2">
+            <Row>
+                <Col md={3} className="picContainer ml-3">
+                    <img src='profilepic.jpg' alt="pic" className="profilePic" />
+                </Col>
+                <Col md={8} className="tmContainer">
+                    <span>{t.name} {t.last_name}</span>
+                    <p>{t.rol}</p>
+                </Col>
+            </Row>
         </div>
     )
 }
