928b24d304c15527fb18f95d2be571a9e5d9fbe1
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-12 15:17:35 -0300

    tabla con useffect

--- ui/src/Config/error.jsx
+++ ui/src/Config/error.jsx
@@ -2,6 +2,10 @@ const ERROR = {
     ACCORDION: {
         ACCORDION_DATA_GET_FAILS:
             "Carga fallida"
+    },
+    CLIENTS: {
+        CLIENTS_DATA_GET_FAILS:
+            "Carga fallida"
     }
 }
 
